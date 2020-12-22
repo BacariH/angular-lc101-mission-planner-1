d8f8524a51a44902d79cf5fc3690f74d0d06e0a8
Grrrdolf <herseyb71@gmail.com>
2020-12-21 19:13:55 -0600

    Completed without bonus

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -7,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
-  crew: string[] = ['Jessica Watkins', 'Raja Chari', 'Jasmine Moghbeli'];
+  crew: object[] = [
+    {name: "Eileen Collins", firstMission: false},
+    {name: "Mae Jemison", firstMission: false},
+    {name: "Ellen Ochoa", firstMission: true}
+ ];
   
+  memberBeingEdited: object = null;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(name: string, isFirst: boolean){
+    if(this.crew.includes(this.crew[0])){
+      alert(`${name} is already chosen.`)
+    } else {
+      this.crew.push({name: name, firstMission: isFirst});
+    }
+    
+  }
+
+  remove(member: object){
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
   }
 
 }
